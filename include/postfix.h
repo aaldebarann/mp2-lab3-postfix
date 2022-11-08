65d9fda9a5bd5d1157e24c19e8f3957328f3ed78
Ivanchenko Aleksei <amivanchenko@yandex.ru>
2022-11-08 21:26:36 +0300

    new features: can parse expression with constants

--- include/postfix.h
+++ include/postfix.h
@@ -1,24 +1,59 @@
 ﻿#ifndef __POSTFIX_H__
 #define __POSTFIX_H__
 
+#include <iostream>
 #include <string>
+#include <map>
 #include "stack.h"
 
 using namespace std;
 
-class TPostfix
-{
-  string infix;
-  string postfix;
+class ArithmeticExpression {
+    // приоритет операций
+    static map<char, int> priority; // все разрешенные операции односимвольные
+    // типы лексем
+    enum lType {number, variable, operation, begin, end, null}; // begin и end - открывающая и закрывающая скобка
+    // проверка символов
+    static bool isDigit(char c); // 0 ... 9
+    static bool isLetter(char c); // a ... z, A ... Z, _
+    static bool isOperation(char c); // +, -, *, /
+    static bool isMinus(char c); // -
+    static bool isPoint(char c); // -
+    static bool isBegin(char c); // (
+    static bool isEnd(char c); // )
+
+    static void deleteAll(string& str, char toDelete) {
+        int spaces = 0;
+        for(int i = 0; i < str.size(); i++) {
+            str[i - spaces] = str[i];
+            if(str[i] == toDelete)
+                spaces++;
+        }
+        str.erase(str.size() - spaces, spaces);
+    }
+
+    string text;
+    vector<pair<lType, string>> lexems; // набор пар (тип_лексемы, текст_лексемы)
+    vector<pair<lType, string>> postfix; // набор пар (тип_лексемы, текст_лексемы)
+    map<string, double> operands;
+
+    void parse(); // текст -> набор лексем
+    void toPostfix();
+    void readOperands(istream& input, ostream& output);
+
 public:
-  TPostfix()
-  {
-    infix = "a + b";
-  }
-  string GetInfix() { return infix; }
-  string GetPostfix() { return postfix; }
-  string ToPostfix();
-  double Calculate(); // Ввод переменных, вычисление по постфиксной форме
+    explicit ArithmeticExpression(const string& text);
+
+    string getInfix() const { return text; }
+    string getPostfix() const {
+        string postfixStr;
+        for(auto& p: postfix) {
+            postfixStr += p.second;
+        }
+        return postfixStr;
+    }
+
+    double Calculate(istream& input = cin, ostream& output = cout); // Ввод переменных, вычисление по постфиксной форме
 };
 
 #endif
