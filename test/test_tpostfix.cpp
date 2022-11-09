113c3cb1eb9ced161e970df604189a382a471bc5
Ivanchenko Aleksei <amivanchenko@yandex.ru>
2022-11-09 17:12:06 +0300

    some fixes

--- test/test_tpostfix.cpp
+++ test/test_tpostfix.cpp
@@ -17,6 +17,7 @@ TEST(ArithmeticExpression, can_parse_brackets_expression) {
     string str = " (  ((2)/4) -(4.0*_var1_ + 0.4) * (.4 / 3.) - f   +vAr_2   - 7)";
     ASSERT_NO_THROW(ArithmeticExpression expression(str));
 }
+
 TEST(ArithmeticExpression, throws_when_empty_string) {
     string str = "";
     ASSERT_ANY_THROW(ArithmeticExpression expression(str));
@@ -45,11 +46,14 @@ TEST(ArithmeticExpression, throws_when_invalid_symbols) {
     string str = "a + 3b^ + 4";
     ASSERT_ANY_THROW(ArithmeticExpression expression(str));
 }
+
 TEST(ArithmeticExpression, can_get_infix)
 {
-  ArithmeticExpression expression("a+b*c-d");
-  EXPECT_EQ("a+b*c-d", expression.getInfix());
+    ArithmeticExpression expression("aaa - 123.23 +b*c/.124-d");
+
+    EXPECT_EQ("aaa-123.23+b*c/.124-d", expression.getInfix());
 }
+
 TEST(ArithmeticExpression, can_get_postfix_equal_priority)
 {
   ArithmeticExpression expression("a+b - c      - d");
@@ -65,6 +69,7 @@ TEST(ArithmeticExpression, can_get_postfix_brackets)
   ArithmeticExpression expression("  a+b*(c -  e)  - d*e + (f+g*(j-i))");
   EXPECT_EQ("abce-*+de*-fgji-*++", expression.getPostfix());
 }
+
 TEST(ArithmeticExpression, can_calculate_addition_vars_only)
 {
     ArithmeticExpression expression(" a + b + (c + d)");
@@ -155,19 +160,22 @@ TEST(ArithmeticExpression, can_calculate_division_repeated_variables_vars_only)
 }
 TEST(ArithmeticExpression, can_calculate_vars_only)
 {
-    ArithmeticExpression expression("( a / b - (c / d) * e + f - g*g) / (a + b)");
-    istringstream values("1 2 3 4 5 6 7");
+    ArithmeticExpression expression("( a / b - (c / d) * e + f ) ");
+    istringstream values("2 4 3 6 5 6");
     ostream nowhere(nullptr);
 
     double result = expression.Calculate(values, nowhere);
-    double expected = (1.0 / 2.0 - (3.0 / 4.0) * 5.0 + 0.6 - 0.7*0.7) / (0.1 + 0.2);
+    double expected = (2.0 / 4.0 - (3.0 / 6.0) * 5.0 + 6.0 ) ;
 
     EXPECT_EQ(expected, result);
 }
+
 TEST(ArithmeticExpression, can_calculate_subtraction_digits_only)
 {
     ArithmeticExpression expression(" 1 - 2 - (3 - 4)");
-    double result = expression.Calculate();
+    ostream nowhere(nullptr);
+
+    double result = expression.Calculate(cin, nowhere);
     double expected = 1 - 2 - (3 - 4);
 
     EXPECT_EQ(expected, result);
@@ -176,7 +184,9 @@ TEST(ArithmeticExpression, can_calculate_multiplication_digits_only)
 {
     ArithmeticExpression expression(" 1 * 2 * (3 * 4)");
 
-    double result = expression.Calculate();
+    ostream nowhere(nullptr);
+
+    double result = expression.Calculate(cin, nowhere);
     double expected = 1 * 2 * (3 * 4);
 
     EXPECT_EQ(expected, result);
@@ -184,8 +194,9 @@ TEST(ArithmeticExpression, can_calculate_multiplication_digits_only)
 TEST(ArithmeticExpression, can_calculate_division_digits_only)
 {
     ArithmeticExpression expression(" 1.0 / 2.0 / (3.0 / 4.0)");
+    ostream nowhere(nullptr);
 
-    double result = expression.Calculate();
+    double result = expression.Calculate(cin, nowhere);
     double expected = 1.0 / 2.0 / (3.0 / 4.0);
 
     EXPECT_EQ(expected, result);
@@ -193,8 +204,9 @@ TEST(ArithmeticExpression, can_calculate_division_digits_only)
 TEST(ArithmeticExpression, can_calculate_digits_only)
 {
     ArithmeticExpression expression("(1.0 / 2.0 - (3.0 / 4.0) * 5.0 + 0.6 - 0.7*0.7) / (0.1 + 0.2)");
+    ostream nowhere(nullptr);
 
-    double result = expression.Calculate();
+    double result = expression.Calculate(cin, nowhere);
     double expected = (1.0 / 2.0 - (3.0 / 4.0) * 5.0 + 0.6 - 0.7*0.7) / (0.1 + 0.2);
 
     EXPECT_EQ(expected, result);
@@ -236,7 +248,7 @@ TEST(ArithmeticExpression, can_calculate_multiplication)
 TEST(ArithmeticExpression, can_calculate_division)
 {
     ArithmeticExpression expression(" a / b / (0.3 / d)");
-    istringstream values("1 2 3 4");
+    istringstream values("1 2 4");
     ostream nowhere(nullptr);
 
     double result = expression.Calculate(values, nowhere);
@@ -247,11 +259,11 @@ TEST(ArithmeticExpression, can_calculate_division)
 TEST(ArithmeticExpression, can_calculate)
 {
     ArithmeticExpression expression("( a / b - (c / d) * e + f - 0.7*g) / (1.0 + 2.0)");
-    istringstream values("1 2 3 4 5 6 7");
+    istringstream values("1 2 3 4 5 0.6 .7");
     ostream nowhere(nullptr);
 
     double result = expression.Calculate(values, nowhere);
-    double expected = (1.0 / 2.0 - (3.0 / 4.0) * 5.0 + 0.6 - 0.7*0.7) / (0.1 + 0.2);
+    double expected = (1.0 / 2.0 - (3.0 / 4.0) * 5.0 + 0.6 - 0.7*0.7) / (1.0 + 2.0);
 
     EXPECT_EQ(expected, result);
 }
